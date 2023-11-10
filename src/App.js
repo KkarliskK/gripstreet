97206375b147a872d15768be3129aae13cec304f
KkarliskK <karlisbirk@gmail.com>
2023-11-10 22:03:29 +0200

    Installed TailwindCss

--- src/App.js
+++ src/App.js
@@ -1,11 +1,9 @@
-import './App.css';
+import React from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      
-    </div>
-  );
+export default function App() {
+    return (
+        <div className='flex flex-col items-center container mx-auto max-w-2xl shadow  rounded border-blue-400 border p-4 my-5'>
+            <span className='text-xl '>🩵 GRIPSTREET LATVIJA 🩵</span>
+        </div>
+    )
 }
-
-export default App;
