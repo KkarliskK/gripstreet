d77c779b5fee91270481ff17b91be339201f5861
KkarliskK <karlisbirk@gmail.com>
2023-11-13 21:13:13 +0200

    MAJOR CHANGES (installed axios, reacrt-router)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import axios from 'axios';
 import React, { useEffect, useState } from 'react';
 import './scss/Main.scss';
+import { useNavigate } from 'react-router-dom';
+import Header from './Components/Header';
 
 export default function App() {
     return (
-        <div className='flex flex-col items-center container mx-auto max-w-2xl shadow  rounded border-blue-400 border p-4 my-5'>
-            <span className='text-xl '>🩵 GRIPSTREET LATVIJA 🩵</span>
-        </div>
+        <>
+            <Header />
+            {/**HERE WE NEED TO MAKE THE MAIN PAGE**/}
+        </>
     )
 }
--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./Routes/Login";
+import App from "./App";
+import Register from "./Routes/Register";
+
+
+export default function Index() {
+  return (
+    <BrowserRouter>
+      <Routes>
+          <Route index element={<App />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode> 
-);
-
+root.render(<Index />);
