6cae0a87dad0af3ae09d839b63699df144fb7f18
KkarliskK <karlisbirk@gmail.com>
2024-01-08 21:10:18 +0200

    Smool updates  v0.0.3
    *Started to work on footer and it's design
    *Made blog/calendar/store/gallery files and associated buttons for them.
    *Also updated the new files into router
    *Fixed other small bugs and errors

--- src/index.js
+++ src/index.js
@@ -3,6 +3,8 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Login from "./Routes/Login";
 import Main from "./Routes/Main";
 import Register from "./Routes/Register";
+import Store from "./Routes/Store";
+import Calendar from "./Routes/Calendar";
 
 
 export default function Index() {
@@ -12,6 +14,8 @@ export default function Index() {
           <Route index element={<Main />} />
           <Route path="/login" element={<Login />} />
           <Route path="/register" element={<Register />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/calendar" element={<Calendar />} />
       </Routes>
     </BrowserRouter>
   );
